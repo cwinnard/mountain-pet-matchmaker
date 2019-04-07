412d860f7e902648448d89f34a1378d31b70e130
Charlie Winnard <charlie@overtone.co>
2019-04-07 13:11:44 -0600

    app bar and styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Card, CardContent, CardMedia } from '@material-ui/core';
+import { AppBar, Card, CardContent, CardMedia } from '@material-ui/core';
 import React, { Component } from 'react';
 import './styles/app.css';
 
@@ -15,29 +15,32 @@ class App extends Component {
     render() {
         const dogInfo = getDogInfo(json);
         return (
-            <div className="app-container">
-                {this.state.phase === 0 && <Quiz />}
-                {this.state.phase > 0 &&
-                    dogInfo.map((info) => (
-                        <Card className="dog-card" key={info.id}>
-                        <CardMedia
-                            component="img"
-                            alt="dog pic"
-                            className="dog-image"
-                            image={info.photos[0].medium}
-                        />
-                            <CardContent>
-                                <div>{info.id}</div>
-                                <div>{info.name}</div>
-                                <div>{info.breeds.primary}</div>
-                                <div>{info.description}</div>
-                                <div>{info.age}</div>
-                                <div>{info.gender}</div>
-                                <div>{info.size}</div>
-                            </CardContent>
-                        </Card>
-                    ))
-                }
+            <div className="app">
+                <AppBar position='absolute' style={{height: '50px'}}/>
+                <div className="app-container">
+                    {this.state.phase === 0 && <Quiz />}
+                    {this.state.phase > 0 &&
+                        dogInfo.map((info) => (
+                            <Card className="dog-card" key={info.id}>
+                            <CardMedia
+                                component="img"
+                                alt="dog pic"
+                                className="dog-image"
+                                image={info.photos[0].medium}
+                            />
+                                <CardContent>
+                                    <div>{info.id}</div>
+                                    <div>{info.name}</div>
+                                    <div>{info.breeds.primary}</div>
+                                    <div>{info.description}</div>
+                                    <div>{info.age}</div>
+                                    <div>{info.gender}</div>
+                                    <div>{info.size}</div>
+                                </CardContent>
+                            </Card>
+                        ))
+                    }
+                </div>
             </div>
         );
     }
