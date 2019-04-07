8fe077c8730621da6f5ed9f4180adce08bb3580d
Charlie Winnard <charlie@overtone.co>
2019-04-07 11:32:02 -0600

    track phase

--- src/App.js
+++ src/App.js
@@ -8,30 +8,36 @@ import getDogInfo from './data/informationGatherer';
 import json from './data/mpr-dogs-3-13.json';
 
 class App extends Component {
+    state = {
+        phase: 0,
+    };
+
     render() {
         const dogInfo = getDogInfo(json);
         return (
             <div className="App">
-                <Quiz />
-                {dogInfo.map((info) => (
-                    <Card className="dog-card" key={info.id}>
-                    <CardMedia
-                        component="img"
-                        alt="dog pic"
-                        className="dog-image"
-                        image={info.photos[0].medium}
-                    />
-                        <CardContent>
-                            <div>{info.id}</div>
-                            <div>{info.name}</div>
-                            <div>{info.breeds.primary}</div>
-                            <div>{info.description}</div>
-                            <div>{info.age}</div>
-                            <div>{info.gender}</div>
-                            <div>{info.size}</div>
-                        </CardContent>
-                    </Card>
-                ))}
+                {this.state.phase === 0 && <Quiz />}
+                {this.state.phase > 0 &&
+                    dogInfo.map((info) => (
+                        <Card className="dog-card" key={info.id}>
+                        <CardMedia
+                            component="img"
+                            alt="dog pic"
+                            className="dog-image"
+                            image={info.photos[0].medium}
+                        />
+                            <CardContent>
+                                <div>{info.id}</div>
+                                <div>{info.name}</div>
+                                <div>{info.breeds.primary}</div>
+                                <div>{info.description}</div>
+                                <div>{info.age}</div>
+                                <div>{info.gender}</div>
+                                <div>{info.size}</div>
+                            </CardContent>
+                        </Card>
+                    ))
+                }
             </div>
         );
     }
