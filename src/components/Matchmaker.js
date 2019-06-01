0acfad0e2b1e6628d8458fcf9099b78f706f85a1
Charlie Winnard <charlie@overtone.co>
2019-06-01 12:33:06 -0600

    hook up redux so sweet

--- src/components/Matchmaker.js
+++ src/components/Matchmaker.js
@@ -1,4 +1,4 @@
-import { AppBar } from '@material-ui/core';
+import { AppBar, Card, CardContent, CardMedia } from '@material-ui/core';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
@@ -15,7 +15,26 @@ class Matchmaker extends Component {
                 <div className="app-container">
                     {phase === 0 && <Quiz />}
                     {phase > 0 &&
-                        <div>hello 2</div>
+                        matches.map((info) => (
+                            <Card className="dog-card" key={info.id}>
+                            <CardMedia
+                                component="img"
+                                alt="dog pic"
+                                className="dog-image"
+                                image={info.photos[0].medium}
+                            />
+                                <CardContent>
+                                    <div>{info.id}</div>
+                                    <div>{info.name}</div>
+                                    <div>{info.breeds.primary}</div>
+                                    <div>{info.description}</div>
+                                    <div>{info.age}</div>
+                                    <div>{info.gender}</div>
+                                    <div>{info.size}</div>
+                                </CardContent>
+                            </Card>
+                        ))
+
                     }
                 </div>
             </div>
